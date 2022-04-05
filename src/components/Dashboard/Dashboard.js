c26c5fbbabe7b5c3387da43c95425cde658f66fd
Abdul <abdulmomin956@gmail.com>
2022-04-05 13:10:22 +0600

    chart completed

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Bar, BarChart, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
+import useChart from '../../hooks/useChart';
+import './Dashboard.css'
 
 const Dashboard = () => {
+    const [data, setData] = useChart();
     return (
-        <div>
-            <h1>This is the Dashboard</h1>
-        </div>
+        <main className='chart-container'>
+            <div className='chart'>
+                <div>
+                    <h4 style={{ textAlign: "center" }}>Our Success Rate</h4>
+                    <LineChart width={600} height={300} data={data}>
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                        <Line dataKey="sell" />
+                        <Tooltip />
+                    </LineChart>
+                </div>
+                <div>
+                    <h4 style={{ textAlign: "center" }}>Branch Increament</h4>
+                    <BarChart width={600} height={300} data={data}>
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                        <Tooltip />
+                        <Bar dataKey="sell" fill="#8884d8" />
+                    </BarChart>
+                </div>
+            </div>
+            <div className='pie'>
+                <h4 style={{ textAlign: "center" }}>Demand  by Age</h4>
+                <PieChart width={730} height={400}>
+                    <Pie data={data} dataKey="sell" nameKey='month' cx="50%" cy="50%" outerRadius={80} fill="blueviolet" />
+                    <Pie data={data} dataKey="sell" nameKey='month' cx="50%" cy="50%" innerRadius={90} outerRadius={120} fill="#82ca9d" label />
+                    <Tooltip />
+                </PieChart>
+            </div>
+
+
+        </main>
     );
 };
 
